6ee563b013d154acee62cb2bfff6e69e0758fb8e
Rusydy <rusydymuhiddin@gmail.com>
2024-01-30 04:37:26 +0700

    Add image to MainActivity and update GreetingText

--- app/src/main/java/com/learn/simple_text_app/MainActivity.kt
+++ app/src/main/java/com/learn/simple_text_app/MainActivity.kt
@@ -3,7 +3,9 @@ package com.learn.simple_text_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,10 +27,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(
-                modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                GreetingText(
+                GreetingImage(
                     message = "Happy Birthday",
                     from = "Android"
                 )
@@ -68,10 +71,31 @@ fun GreetingText(
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "Android Party",
+            modifier = modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = modifier
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview
 @Composable
 fun BirthdayCardPreview() {
-    GreetingText(
+    GreetingImage(
         message = "Happy Birthday Sam!",
         from = "From Android"
     )
